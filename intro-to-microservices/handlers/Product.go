bed3396ef205d661338cf7cb92b8c7e1e0ede104
Nick Gowdy <nickgowdy@nicks-mbp.home>
2022-03-20 12:17:42 +0000

    More progress in course.

---
+++ intro-to-microservices/handlers/Product.go
@@ -0,0 +1,31 @@
+package handlers
+
+import (
+	"intro-to-microservices/product-api/data"
+	"log"
+	"net/http"
+)
+
+type Products struct {
+	l *log.Logger
+}
+
+func NewProducts(l *log.Logger) *Products {
+	return &Products{l}
+}
+
+func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		p.GetProducts(rw, r)
+		return
+	}
+	rw.WriteHeader(http.StatusMethodNotAllowed)
+}
+
+func (p *Products) GetProducts(rw http.ResponseWriter, h *http.Request) {
+	lp := data.GetProducts()
+	err := lp.ToJson(rw)
+	if err != nil {
+		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
+	}
+}
---
+++ intro-to-microservices/product-api/data/product.go
@@ -0,0 +1,50 @@
+package data
+
+import (
+	"encoding/json"
+	"io"
+	"time"
+)
+
+type Product struct {
+	ID          int     `json:"Id"`
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Price       float32 `json:"price"`
+	SKU         string  `json:"sku"`
+	CreatedOn   string  `json:"-"`
+	UpdatedOn   string  `json:"-"`
+	DeletedOn   string  `json:"-"`
+}
+
+type Products []*Product
+
+func (p *Products) ToJson(w io.Writer) error {
+	e := json.NewEncoder(w)
+	return e.Encode(p)
+}
+
+func GetProducts() Products {
+	return productList
+}
+
+var productList = []*Product{
+	&Product{
+		ID:          1,
+		Name:        "Latter",
+		Description: "Frosty milky coffee",
+		Price:       2.45,
+		SKU:         "acb123",
+		CreatedOn:   time.Now().UTC().String(),
+		UpdatedOn:   time.Now().UTC().String(),
+	},
+	&Product{
+		ID:          2,
+		Name:        "Espresso",
+		Description: "Short and strong coffee without milk",
+		Price:       1.99,
+		SKU:         "fjd34",
+		CreatedOn:   time.Now().UTC().String(),
+		UpdatedOn:   time.Now().UTC().String(),
+	},
+}
