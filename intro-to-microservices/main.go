4e4113b80e02edce73c541109c23cbaad7210422
Nick Gowdy <nickgowdy@Nicks-MacBook-Pro.local>
2022-03-20 10:43:19 +0000

    More progress in course.

--- intro-to-microservices/main.go
+++ intro-to-microservices/main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"time"
 )
 
@@ -19,16 +20,28 @@ func main() {
 	sm.Handle("/goodbye", gh)
 
 	s := &http.Server{
-		Addr:         "9090",
+		Addr:         ":9090",
 		Handler:      sm,
 		IdleTimeout:  120 * time.Second,
 		ReadTimeout:  1 * time.Second,
 		WriteTimeout: 1 * time.Second,
 	}
 
-	s.ListenAndServe()
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
 
-	tc := context.WithDeadline(context.Background(), 30*time.Second)
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <-sigChan
+	l.Println("Recieved terminate, graceful shutdown", sig)
+
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
 	s.Shutdown(tc)
 
 }
