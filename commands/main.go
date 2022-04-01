fe245415412af7c7972ddf8ddb0649ffd9a67208
Nick Gowdy <nickgowdy87@gmail.com>
2022-04-01 10:35:17 +0100

    more progress.

---
+++ commands/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		i := i
+		go func() {
+			fmt.Println(i)
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+}
